ff5bb25b9d53a6ce084c6216fae0739559b2b688
Fasih Hussain <fasih.hussain@securiti.ai>
2023-03-30 22:59:49 +0500

    add sample output

--- 03-patterns.py
+++ 03-patterns.py
@@ -1,3 +1,8 @@
+# *****
+# *   *
+# *   *
+# *   *
+# *****
 def print_square(n):
     if n < 2:
         print('"n" should be greater than 2')
@@ -8,6 +13,11 @@ def print_square(n):
     print("*" * n)
 
 
+# *
+# **
+# ***
+# ****
+# *****
 def print_right_triangle(n):
     if n < 0:
         print('"n" should be greater than 0')
@@ -16,6 +26,11 @@ def print_right_triangle(n):
         print("*" * i)
 
 
+#     *
+#    **
+#   ***
+#  ****
+# *****
 def print_mirrored_right_triangle(n):
     if n < 0:
         print('"n" should be greater than 0')
@@ -24,6 +39,15 @@ def print_mirrored_right_triangle(n):
         print(" " * (n - i) + "*" * i)
 
 
+#       *
+#      ***
+#     *****
+#    *******
+#   *********
+#    *******
+#     *****
+#      ***
+#       *
 # https://www.codingem.com/diamond-pattern-in-python-using-for-loop/
 def print_diamond(n):
     for i in range(n):
